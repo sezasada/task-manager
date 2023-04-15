4111763ec59313351c16480c0636ae39ed0bc536
Sebastien Zasada <sezasada@icloud.com>
2023-04-15 17:08:02 -0500

    adjusted some of the data being displayed

--- src/components/Pages/MainPage/TabComponents/Admin/AdminDashboard.jsx
+++ src/components/Pages/MainPage/TabComponents/Admin/AdminDashboard.jsx
@@ -1,7 +1,6 @@
 import { useSelector } from "react-redux";
 import { Stack } from "@mui/system";
 import { Typography, Box } from "@mui/material";
-import TasksAwaitingApproval from "./AdminDashboard/TasksAwaitingApproval";
 import TasksAssignedToYou from "./AdminDashboard/TasksAssignedToYou";
 
 export default function AdminDashboard() {
@@ -12,10 +11,6 @@ export default function AdminDashboard() {
       spacing={3}
       sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
     >
-      <Box>
-
-        <TasksAwaitingApproval />
-      </Box>
       <Box>
         <TasksAssignedToYou />
       </Box>
